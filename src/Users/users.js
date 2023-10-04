0755d155291ec5f5caccb70b00196af8f20015cd
rmnpn <romanpincak74@gmail.com>
2023-10-04 14:52:06 +0300

    5 домашка 1 частина

--- src/Users/users.js
+++ src/Users/users.js
@@ -2,11 +2,11 @@ import React, {useEffect, useState} from 'react';
 import {userService} from "./userService";
 import User from "./User/user";
 
-const Users = () => {
+const Users = ({setOnSave}) => {
     const [users, setUsers] = useState([]);
     useEffect(() => {
         userService.getAll().then(value => value.data).then(data=> setUsers(data))
-    }, []);
+    }, [setOnSave]);
     return (
         <div>
             {users.map(user => (<User key={user.id} user={user}/>))}
