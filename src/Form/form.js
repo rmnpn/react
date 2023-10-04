8b1bbaeb21234b7eb05909faecc308698167e035
rmnpn <romanpincak74@gmail.com>
2023-10-04 14:24:38 +0300

    5 домашка 1 частина

--- src/Form/form.js
+++ src/Form/form.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useForm} from "react-hook-form";
+import axios from "axios";
 
 const Form = () => {
     const {register, handleSubmit, reset, setValue} = useForm();
     const save = ()=> {
-
+        useEffect(() => {
+            axios.create(save)
+        }, []);
     }
     return (
-        <form onSubmit={handleSubmit()}>
+        <form onSubmit={handleSubmit(save)}>
             <label>
                  <input type={"text"} placeholder={'name'}{...register('name')}/>
                  <input type={"text"} placeholder={'username'}{...register('username')}/>
