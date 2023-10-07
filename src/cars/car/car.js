94f6326a293c12c09809fa1d65812feba407373d
rmnpn <romanpincak74@gmail.com>
2023-10-07 17:50:49 +0300

    5 домашка 3 частина

--- src/cars/car/car.js
+++ src/cars/car/car.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const Car = ({car}) => {
     const {id,brand,price,year} = car;
+
+    }
     return (
         <div>
             <div> id:{id}</div>
