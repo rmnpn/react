f44e407b5b89a68caa248fcbb83954db77723999
rmnpn <romanpincak74@gmail.com>
2024-01-04 16:02:12 +0200

    home work rm toolkit

--- src/components/charactersContainer/characters/characters.js
+++ src/components/charactersContainer/characters/characters.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {characterActions} from "../../../redux";
 import Character from "../character/character";
+import {characterActions} from "../../../redux/slices/charactersSlice";
 
 const Characters = () => {
     const dispatch = useDispatch();
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit";
-import {characterReducer} from "./slices";
+import {characterReducer} from "./slices/charactersSlice";
 
 const store = configureStore({
     reducer: {
